8566e7a799659355005917cad9dbb41adcd5a716
Scott <scott_hoffmann1879@outlook.com>
2022-05-03 17:20:29 -0400

    diet edit fix

--- src/Components/Tables/DietTable.js
+++ src/Components/Tables/DietTable.js
@@ -1,168 +1,176 @@
-import React from 'react'
+import React from 'react';
 import MaterialTable from 'material-table';
 import { alpha } from '@material-ui/core/styles';
-import Axios from 'axios'
+import Axios from 'axios';
 
-const DietTable = ({tableData, selectedDiet, dietNum, userId, setTableData, setError, columns, selectedRow, setSelectedRow}) => {
-return (
-<MaterialTable
-              components={{
-                Groupbar: props => (
-                  <div style={{width: "0px"}}></div>
-                ),
-                Grouprow: props => (
-                  <div style={{width: "0px"}}></div>
-                )
+const DietTable = ({
+  tableData,
+  selectedDiet,
+  dietNum,
+  userId,
+  setTableData,
+  setError,
+  columns,
+  selectedRow,
+  setSelectedRow,
+}) => {
+  return (
+    <MaterialTable
+      components={{
+        Groupbar: (props) => <div style={{ width: '0px' }}></div>,
+        Grouprow: (props) => <div style={{ width: '0px' }}></div>,
+      }}
+      editable={{
+        onRowAdd: (newRow) =>
+          new Promise((resolve, reject) => {
+            let tempTable = [...selectedDiet.food[dietNum].data, newRow];
+            let tempData = selectedDiet.food;
+            tempData[dietNum].data = tempTable;
 
-              }}
-              editable={{
-                onRowAdd:(newRow) =>new Promise((resolve, reject) => {
-                  let tempTable = [...selectedDiet.food[dietNum].data, newRow];
-                  let tempData = selectedDiet.food
-                  tempData[dietNum].data = tempTable
+            const sendUpdate = async () => {
+              let results;
+              try {
+                results = await Axios.patch(
+                  'http://localhost:5000/api/diets/edit/',
+                  {
+                    userId: userId,
+                    food: tempData,
+                    dietId: selectedDiet.id,
+                  }
+                );
+              } catch (err) {
+                setError(err);
+                return;
+              }
+            };
+            sendUpdate();
 
-                  const sendUpdate = async () => {
-                    let results;
-                    try {
-                      results = await Axios.patch(
-                        'http://localhost:5000/api/diets/edit/',
-                        {
-                          userId: userId,
-                          food: tempData,
-                          dietId: selectedDiet.id
-                        }
-                      );
-                    } catch (err) {
-                      setError(err);
-                      return;
-                    }
-                  };
-                  sendUpdate();
+            setTableData([...tableData, newRow]);
+            setTimeout(() => resolve(), 500);
+          }),
 
+        onRowUpdate: (newRow, oldRow) =>
+          new Promise((resolve, reject) => {
+            let updatedData = [...selectedDiet.food[dietNum].data];
+            updatedData[oldRow.tableData.id] = newRow;
+            let tempData = selectedDiet.food;
+            tempData[dietNum].data = updatedData;
+            console.log(tempData)
+            const sendUpdate = async () => {
+              let results;
+              try {
+                results = await Axios.patch(
+                  'http://localhost:5000/api/diets/edit/',
+                  {
+                    userId: userId,
+                    dietId: selectedDiet.id,
+                    food: tempData,
+                  }
+                );
+              } catch (err) {
+                setError(err);
+                return;
+              }
+            };
+            sendUpdate();
+            setTableData(updatedData);
+            setTimeout(() => resolve(), 500);
+          }),
 
-                  setTableData([...tableData, newRow])
-                  setTimeout(()=>resolve(), 500)
-                }),
-                onRowUpdate:(newRow,oldRow)=> new Promise((resolve, reject) => {
-                  let updatedData = [...selectedDiet.food[dietNum].data]
-                  updatedData[oldRow.tableData.id] = newRow
-                  let tempData = selectedDiet.food
-                  tempData[dietNum].data = updatedData
-                  const sendUpdate = async () => {
-                    let results;
-                    try {
-                      results = await Axios.patch(
-                        'http://localhost:5000/api/workouts/edit/',
-                        {
-                          userId: userId,
-                          dietId: selectedDiet.id,
-                          food: tempData
-                        }
-                      );
-                    } catch (err) {
-                      setError(err);
-                      return;
-                    }
-                  };
-                  sendUpdate();
-                  setTableData(updatedData)
-                  setTimeout(()=>resolve(), 500)
-                }),
+        onRowDelete: (selectedRow) =>
+          new Promise((resolve, reject) => {
+            let updatedData = [...selectedDiet.food[dietNum].data];
+            updatedData.splice(selectedRow.tableData.id, 1);
+            let tempData = selectedDiet.food;
+            tempData[dietNum].data = updatedData;
+            const sendUpdate = async () => {
+              let results;
+              try {
+                results = await Axios.patch(
+                  'http://localhost:5000/api/diets/edit/',
+                  {
+                    userId: userId,
+                    dietId: selectedDiet.id,
+                    food: tempData,
+                  }
+                );
+              } catch (err) {
+                setError(err);
+                return;
+              }
+            };
+            sendUpdate();
 
+            setTableData(updatedData);
+            setTimeout(() => resolve(), 500);
+          }),
 
+        onBulkUpdate: (selectedRows) =>
+          new Promise((resolve, reject) => {
+            let rows = Object.values(selectedRows);
+            let updatedRows = [...selectedDiet.food[dietNum].data];
+            let index;
+            rows.map((emp) => {
+              index = emp.oldData.tableData.id;
+              updatedRows[index] = emp.newData;
+            });
 
+            let tempData = selectedDiet.food;
+            tempData[dietNum].data = updatedRows;
+            const sendUpdate = async () => {
+              let results;
+              try {
+                results = await Axios.patch(
+                  'http://localhost:5000/api/diets/edit/',
+                  {
+                    userId: userId,
+                    dietId: selectedDiet.id,
+                    food: tempData,
+                  }
+                );
+              } catch (err) {
+                setError(err);
+                return;
+              }
+            };
+            sendUpdate();
 
-                onRowDelete:(selectedRow)=> new Promise((resolve, reject)=> {
-                  let updatedData=[...selectedDiet.food[dietNum].data]
-                  updatedData.splice(selectedRow.tableData.id, 1)
-                  let tempData = selectedDiet.food;
-                  tempData[dietNum].data = updatedData
-                  const sendUpdate = async () => {
-                    let results;
-                    try {
-                      results = await Axios.patch(
-                        'http://localhost:5000/api/workouts/edit/',
-                        {
-                          userId: userId,
-                          dietId: selectedDiet.id,
-                          food: tempData
-                        }
-                      );
-                    } catch (err) {
-                      setError(err);
-                      return;
-                    }
-                  };
-                  sendUpdate();
+            setTimeout(() => {
+              setTableData(updatedRows);
+              resolve();
+            }, 500);
+          }),
+      }}
+      title={selectedDiet.food[dietNum].name}
+      columns={columns}
+      data={tableData}
+      onRowClick={(evt, selectedRow) =>
+        setSelectedRow(selectedRow.tableData.id)
+      }
+      options={{
+        exportAllData: true,
+        exportFileName: 'TableData',
+        addRowPosition: 'last',
+        actionsColumnIndex: -1,
+        tableLayout: 'fixed',
+        search: false,
+        paging: false,
+        exportButton: true,
+        headerStyle: {
+          backgroundColor: '#828282',
+          color: 'white',
+          fontWeight: 'bold',
+        },
+        // grouping: true,
+        rowStyle: (rowData) => ({
+          backgroundColor:
+            selectedRow === rowData.tableData.id ? '#EEE' : '#FFF',
+        }),
 
-
-                  setTableData(updatedData)
-                  setTimeout(()=>resolve(), 500)
-                }),
-
-
-                onBulkUpdate: selectedRows => new Promise((resolve, reject)=> {
-                  let rows = Object.values(selectedRows)
-                  let updatedRows=[...selectedDiet.food[dietNum].data]
-                  let index;
-                  rows.map(emp => {
-                    index = emp.oldData.tableData.id
-                    updatedRows[index]=emp.newData
-                  })
-
-                  let tempData = selectedDiet.food;
-                  tempData[dietNum].data = updatedRows
-                  const sendUpdate = async () => {
-                    let results;
-                    try {
-                      results = await Axios.patch(
-                        'http://localhost:5000/api/workouts/edit/',
-                        {
-                          userId: userId,
-                          dietId: selectedDiet.id,
-                          food: tempData
-                        }
-                      );
-                    } catch (err) {
-                      setError(err);
-                      return;
-                    }
-                  };
-                  sendUpdate();
-
-
-                  setTimeout(() => {
-                    setTableData(updatedRows)
-                    resolve()
-                  }, 500)
-
-                })
-
-              }}
-
-              title={selectedDiet.food[dietNum].name}
-              columns={columns}
-              data={tableData}
-              onRowClick={((evt, selectedRow) => setSelectedRow(selectedRow.tableData.id))}
-              options={{
-                exportAllData: true, exportFileName: "TableData", addRowPosition: "last", actionsColumnIndex: -1,
-                tableLayout: 'fixed',
-                search: false,
-                paging: false,
-                exportButton: true,
-                headerStyle: { backgroundColor: "#828282", color: "white", fontWeight: "bold" },
-                // grouping: true,
-                rowStyle: rowData => ({
-                  backgroundColor: (selectedRow === rowData.tableData.id) ? '#EEE' : '#FFF'
-                })
-
-
-
-                // padding: "dense",
-
-              }}
-            />
-)
-}
+        // padding: "dense",
+      }}
+    />
+  );
+};
 
 export default DietTable;
